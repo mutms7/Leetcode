d077e704faeeac473e834c75107f0162675b3a66
mutms7 <welcomehomemutms7@gmail.com>
2025-08-17 23:55:41 -0700

    m

--- Leetcode/src/aMediumQuestions.java
+++ Leetcode/src/aMediumQuestions.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class aMediumQuestions {
     
 	public static void main(String[] args) {
@@ -18,26 +20,56 @@ public class aMediumQuestions {
         return reversed;
     }
 
-    //P128
+    //P128 YES
     public static int longestConsecutive(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
-        if (nums.length == 1) {
-            return 1;
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            hs.add(nums[i]);
         }
+        List<Integer> sortedList = new ArrayList<>(hs);
+        Collections.sort(sortedList);
         int max = 1;
-        int current = nums[0];
         int count = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == current+count) {
+        for (int i = 0; i < sortedList.size(); i++) {
+            if (sortedList.get(i) == sortedList.get(i-1) + 1) {
                 count++;
+                max = Math.max(count, max);
             } else {
-                max = Math.max(max, count);
                 count = 1;
-                current = nums[i];
             }
         }
         return max;
+        
+    }
+
+    //P659 
+    public static boolean isPossible(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] += 1000;
+        }
+        Map<Integer, Integer> mp = new HashMap<>(); //make map a list that can do contains AND find the i th item
+        for (int i : nums) {
+            if (mp.containsKey(i)) {
+                mp.put(i, mp.get(i) + 1);
+            } else {
+                mp.put(i, 1);
+            }
+        }
+
+        while (!mp.isEmpty()) {
+            int length = 0;
+            int current = -10;
+            LinkedList<Integer> remove = new LinkedList<>();
+            for (int i = 0; i < mp.size(); i++) {
+                if (length == 0) { //need to find a way to get the i th item in the list
+                    current = mp.values().iterator().next();
+                }
+            }
+        }
+
+        return true;
     }
 }
