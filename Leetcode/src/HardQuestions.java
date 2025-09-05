40a404e9524bc10c5665d91b331c9dd783d266ab
mutms7 <welcomehomemutms7@gmail.com>
2025-09-05 10:54:41 -0400

    hello

--- Leetcode/src/HardQuestions.java
+++ Leetcode/src/HardQuestions.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class HardQuestions {
 
     public static void main(String[] args) {
-        System.out.println(test("hello my fellow bellow"));
+        System.out.println(trap(new int[] {4,2,0,3,2,5}));
     }
 
     // PX
@@ -12,5 +12,31 @@ public class HardQuestions {
         return "";
     }
 
-    
+    //P42
+    public static int trap(int[] height) {
+        int l = 0;
+        int r = height.length-1;
+        int lh = 0;
+        int rh = 0;
+        int water = 0;
+        while (l <= r) {
+            if (lh >= rh) { //l is taller
+                if (rh <= height[r]) {
+                    rh = height[r];
+                } else {
+                    water += rh - height[r];
+                }
+                r--;
+            } else { //r is taller
+                if (lh <= height[l]) {
+                    lh = height[l];
+                } else {
+                    water += lh - height[l];
+                }
+                l++;
+            }
+        }
+
+        return water;
+    }
 }
