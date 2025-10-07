6c7b78e1d9df1f42f1b58d71be4f20e29322705a
mutms7 <welcomehomemutms7@gmail.com>
2025-10-07 17:39:48 -0400

    w

--- Leetcode/src/aMediumQuestions.java
+++ Leetcode/src/aMediumQuestions.java
@@ -12,7 +12,16 @@ public class aMediumQuestions {
         System.out.println("BANC".substring(0, 3));
     }
     
-    
+    // P53
+    public static int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int current = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            current = Math.max(nums[i], current + nums[i]);
+            max = Math.max(max, current);
+        }
+        return max;
+    }
 
     // P209
     public static int minSubArrayLen(int target, int[] nums) {
